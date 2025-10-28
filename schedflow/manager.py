58076e6edcb568d87a7f9481e4aece3f0d6db2d0
Yi Pan <conlesspan@outlook.com>
2025-10-28 15:10:07 -0700

    perf: temporarily use original callable for non-split case.

--- schedflow/manager.py
+++ schedflow/manager.py
@@ -166,9 +166,20 @@ class SchedFlowManager:
         regular, synchronous forward API.
         """
         assert self.initialized
+        from vllm.forward_context import get_forward_context
 
         def _forward(*args, **kwargs) -> Any:
-            assert self.cached_config is not None
+            assert self.cached_config is not None and self.graph_module is not None
+            if (
+                not self.cached_config.is_dryrun
+                and self.cached_config.num_nano_batches == 1
+            ):
+                forward_context = get_forward_context()
+                assert forward_context is not None and isinstance(
+                    forward_context.attn_metadata, list
+                )
+                forward_context.attn_metadata = forward_context.attn_metadata[0]
+                return self.graph_module(*args, **kwargs)
             result = asyncio.run(self._forward_async(args, kwargs))
             self.cached_config = None
             return result
